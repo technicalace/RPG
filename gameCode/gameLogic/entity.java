13b2b0efbd9d48e6949cae265c46f14a05b0abfd
technicalace <80608826+technicalace@users.noreply.github.com>
2022-07-16 20:48:54 -0500

    Add: new variable (maxHp) within entity class

--- gameCode/gameLogic/entity.java
+++ gameCode/gameLogic/entity.java
@@ -2,11 +2,12 @@ package gameCode.gameLogic;
 
 public class entity {
     public String name; 
-    public int hp, dmg;
+    public int hp, dmg, maxHp;
     
-    public entity(String name, int hp, int dmg) {
+    public entity(String name, int hp, int dmg, int maxHp) {
         this.name = name; 
         this.hp = hp; 
         this.dmg = dmg;
+        this.maxHp = maxHp; 
     }
 }
